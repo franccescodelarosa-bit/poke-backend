f83064c799422012718672da3b5da68751882b2c
Frank De La Rosa <frankdelarosacampos@gmail.com>
2026-01-29 21:29:08 -0500

    permission

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || 'dev_jwt_secret',
+      secretOrKey: process.env.JWT_SECRET || 'ULTRAPASS_1234',
     });
   }
 
